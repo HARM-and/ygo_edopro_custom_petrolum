cdf1c44d74333318387f4d414f7dcac4f40b9477
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-08-10 15:00:39 +0200

    Update c511027102.lua

--- script/c511027102.lua
+++ script/c511027102.lua
@@ -1,4 +1,70 @@
---Diappo, Caffey Barista
-function c511027102.initial_effect(c)
-	
+-- Diappo, Caffey Barista
+local s,id=GetID()
+function s.initial_effect(c)
+    -- Effet d'ajout depuis le Deck
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCountLimit(1,id)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
+    c:RegisterEffect(e1)
+
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+
+    -- Effet d'invocation spéciale depuis la cimetière en Position de Défense (effet d'ignition)
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,id+100)
+    e3:SetCondition(s.spcon)
+    e3:SetTarget(s.sptg)
+    e3:SetOperation(s.spop)
+    c:RegisterEffect(e3)
+end
+-- Effet d'ajout depuis le Deck
+function s.thfilter(c)
+    return c:IsSetCard(0xCAF) and c:IsType(TYPE_MONSTER) and not c:IsCode(id) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoHand(g,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-tp,g)
+    end
+end
+
+-- Effet d'invocation spéciale depuis le cimetière en Position de Défense (effet d'ignition)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==1
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVEYARD,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVEYARD)
+end
+
+function s.filter(c,e,tp)
+    return c:IsSetCard(0xCAF) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVEYARD,0,1,1,nil,e,tp)
+    if #g>0 then
+        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+    end
 end
