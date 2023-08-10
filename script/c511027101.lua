0648b5af150a1d42f37c250c00cc259f27d7afd3
HARMand <84624968+HARM-and@users.noreply.github.com>
2023-08-10 13:35:27 +0200

    Update c511027101.lua

--- script/c511027101.lua
+++ script/c511027101.lua
@@ -48,7 +48,7 @@ end
 
 -- Effet d'invocation spéciale depuis la main en Position de Défense
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0xCAF),tp,LOCATION_MZONE,0,1,nil)
+    return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0xCAF),tp,LOCATION_MZONE,0,1,nil)
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
